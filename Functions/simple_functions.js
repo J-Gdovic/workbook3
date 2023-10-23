2e65ea0afb9f818ed8800648a54c9418fb18e621
J Gdovic <jgdovic3@gmail.com>
2023-10-23 14:32:23 -0400

    simple_functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+function favoriteThings() {
+  let myName ="My Name is : " + "Javier Gdoivc";
+  let favoriteMovie = "My favorite movie is: " + "Ace Ventura (Both Movies)";
+  let favoriteMusician = "My favorite musician is: " + "BigBabyGucci";
+  let favoritThings = myName + " " + favoriteMovie + " " + favoriteMusician;
+  console.log(favoritThings);
+}
+favoriteThings();
+
+function whyImHere() {
+  let whyImHere = "Im here to make a career change and prove to myself that I can learn a new skill.";
+  console.log(whyImHere)
+}
+whyImHere()
+
+function favoritePlace(){
+    let favoritePlace = "The Bahamas is my favorite place, I visted there on a cruise when I was younger and I loved the weather and the beaches."
+    console.log(favoritePlace)
+}
+favoritePlace()
