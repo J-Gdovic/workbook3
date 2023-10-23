b5c2286d516bb7203f5681b49b62da414ade21df
J Gdovic <jgdovic3@gmail.com>
2023-10-23 16:26:58 -0400

    more_functions

---
+++ more_functions.js
@@ -0,0 +1,8 @@
+"use strict";
+displayMailingLable("Javier Gdovic", "5615 Phillips ave. apt.#4", "Pittsburgh", "15217");
+
+function displayMailingLable(myName, myAddress, myCity, myState, myZip) {
+
+    console.log(myAddress)
+}
+
