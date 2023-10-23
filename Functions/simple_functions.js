1cd2d6d9b98c9d4018523e22b06ba11916badf42
J Gdovic <jgdovic3@gmail.com>
2023-10-23 15:26:16 -0400

    added space to simple_functions

--- Functions/simple_functions.js
+++ Functions/simple_functions.js
@@ -1,21 +1,25 @@
 "use strict";
-function favoriteThings() {
-  let myName ="My Name is : " + "Javier Gdoivc";
+function displayFavoriteThings() {
+  let myName = "My Name is : " + "Javier Gdoivc";
   let favoriteMovie = "My favorite movie is: " + "Ace Ventura (Both Movies)";
   let favoriteMusician = "My favorite musician is: " + "BigBabyGucci";
   let favoritThings = myName + " " + favoriteMovie + " " + favoriteMusician;
   console.log(favoritThings);
+  console.log();
 }
-favoriteThings();
+displayFavoriteThings();
 
-function whyImHere() {
+function displayWhyImHere() {
   let whyImHere = "Im here to make a career change and prove to myself that I can learn a new skill.";
-  console.log(whyImHere)
+  console.log(whyImHere);
+  console.log();
 }
-whyImHere()
+displayWhyImHere();
 
-function favoritePlace(){
-    let favoritePlace = "The Bahamas is my favorite place, I visted there on a cruise when I was younger and I loved the weather and the beaches."
-    console.log(favoritePlace)
+function displayfavoritePlace() {
+  let favoritePlace =
+    "The Bahamas is my favorite place, I visted there on a cruise when I was younger and I loved the weather and the beaches.";
+  console.log(favoritePlace);
+  console.log();
 }
-favoritePlace()
+displayfavoritePlace();
