4d9609f2bc15af7a50b8d8dc8a0ffe00f4027fbc
J Gdovic <jgdovic3@gmail.com>
2023-10-24 11:29:26 -0400

    more_functions

--- more_functions.js
+++ more_functions.js
@@ -1,8 +1,21 @@
 "use strict";
-displayMailingLable("Javier Gdovic", "5615 Phillips ave. apt.#4", "Pittsburgh", "15217");
 
 function displayMailingLable(myName, myAddress, myCity, myState, myZip) {
-
-    console.log(myAddress)
+  console.log(myName, myAddress, myCity, myState, myZip);
 }
+displayMailingLable("Javier Gdovic", "5615 Phillips ave. apt.#4", "Pittsburgh", "Pa", "15217");
 
+/////
+
+function addNumbers(x, y) {
+  console.log(x + y);
+}
+addNumbers(5, 7);
+addNumbers(3, 3);
+//////
+
+function displayReceipt(amountPaid, totalDue) {
+  let changeDue = amountPaid - totalDue;
+  console.log(changeDue.toFixed(2) + " : Change Due");
+}
+displayReceipt(40.00, 35.76);
