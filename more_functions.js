b305be8ec3b18bb5c569d3d7f459ee6b07ba0acf
J Gdovic <jgdovic3@gmail.com>
2023-10-24 12:00:20 -0400

    spacing

--- more_functions.js
+++ more_functions.js
@@ -2,14 +2,16 @@
 
 /////#1
 
+
 function displayMailingLable(myName, myAddress, myCity, myState, myZip) {
   console.log(myName);
   console.log(myAddress);
   console.log(myCity + ",",myState,myZip)
 }
 displayMailingLable("Javier Gdovic", "5615 Phillips Ave. apt.#4", "Pittsburgh", "Pa", "15217");
-displayMailingLable("Kenny Pickett", "412 Heinz Ave.", "Pittsburgh", "Pa", "4128214")
-
+console.log()
+displayMailingLable("Kenny Pickett", "412 Heinz Ave.", "Pittsburgh", "Pa", "4128214");
+console.log()
 
 /////#2
 
@@ -17,7 +19,9 @@ function addNumbers(x, y) {
   console.log(x + y);
 }
 addNumbers(5, 7);
+console.log()
 addNumbers(3, 3);
+console.log()
 
 
 //////#3
@@ -27,5 +31,7 @@ function displayReceipt(amountPaid, totalDue) {
   console.log(changeDue.toFixed(2) + " : Change Due");
 }
 displayReceipt(40.00, 35.76);
+console.log()
 displayReceipt(50.00,50.00);
+console.log()
 displayReceipt(45.76,60.00)
