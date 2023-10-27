5e4aab83cb72751e712292dd5aa7f3a801e77e72
J Gdovic <jgdovic3@gmail.com>
2023-10-27 11:45:13 -0400

    toggle paragraph function

--- checkbox/scripts/checkbox.js
+++ checkbox/scripts/checkbox.js
@@ -6,7 +6,7 @@ const displayButton = document.getElementById("displayButton");
 
 //write functions that will handle (be called) when events on the page occur (event handlers)
 function display(){
-    // alert is the action in this function/ .checked shows true or false
+    // alert is the action in this function/ .checked shows true or false of the alert function
 alert(activeCheckbox.checked);
 }
 
---
+++ checkbox/scripts/hideshow.js
@@ -0,0 +1,28 @@
+"use strict";
+//get elements in variables
+let clickH3 = document.getElementById("clickH3");
+let detailsPagrpaph = document.getElementById("detailsPagrpaph");
+
+
+//write function to handle events
+function toggleParagraph() {
+
+    let display = detailsPagrpaph.style.display
+    
+    if (display == "none"){
+        detailsPagrpaph.style.display = "block";
+    }
+    
+    else{
+        detailsPagrpaph.style.display = "none";
+    }
+    
+
+
+
+    
+}
+
+// connect the functions to the  events(wire up)
+clickH3.onclick = toggleParagraph;
+
