cfc9d0fddd43bcd6af8835f86b31ec75f2f9dbb0
J Gdovic <jgdovic3@gmail.com>
2023-10-27 10:40:07 -0400

    textbox alert true or false onclick

---
+++ checkbox/scripts/checkbox.js
@@ -0,0 +1,14 @@
+"use strict";
+// get html elements into variables
+const activeCheckbox = document.getElementById ("activeCheckbox");
+const displayButton = document.getElementById("displayButton");
+
+
+//write functions that will handle (be called) when events on the page occur (event handlers)
+function display(){
+    // alert is the action in this function/ .checked shows true or false
+alert(activeCheckbox.checked);
+}
+
+// (this is the connection to the button) displays the function "display" - dont use ()
+displayButton.onclick = display;
