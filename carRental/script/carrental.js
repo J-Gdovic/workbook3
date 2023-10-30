4148cce56daf65d078f815a7f32ebf8cd96120ca
J Gdovic <jgdovic3@gmail.com>
2023-10-30 11:49:56 -0400

    calculations work

--- carRental/script/carrental.js
+++ carRental/script/carrental.js
@@ -11,37 +11,52 @@ let noRadio = document.getElementById("noRadio");
 let yesRadio = document.getElementById("yesRadio");
 //button
 let estimatedCostButton = document.getElementById("estimatedCostButton")
+//totalseperated
+let carRentalCostParagraph = document.getElementById("carRentalCostParagraph")
+let optionsCostParagraph = document.getElementById("optionsCostParagraph")
+let under25SurchargeParagraph = document.getElementById("under25SurchargeParagraph")
+let totalDueParagraph = document.getElementById("totalDueParagraph")
 
 //make function for event
 function estimateTotalCost() {
     let days = Number(numberOfDaysTextbox.value)
     let costPerDay = 29.99
     let carRentalTotal = days*costPerDay
+    let optionsCost = 0;
+ let under25cost = 0;
+
     
   // add if statement for tolltag + total cost
 if(tollTagCheckbox.checked){
-  carRentalTotal = carRentalTotal + (3.95*days)
+  optionsCost =   (3.95*days)
 }
 
 
 //add if statement for gps + total cost
 if(gpsCheckbox.checked){
-  carRentalTotal = carRentalTotal + (2.95*days)
+  optionsCost =  (2.95*days)
 }
 
 //add if statement for roadsideassistance + total cost
 if(roadSideAssistanceCheckbox.checked){
-carRentalTotal = carRentalTotal + (2.95*days)
+optionsCost = (2.95*days)
 }
 
 //add if statement for under 25
 if(yesRadio.checked){
-  carRentalTotal = carRentalTotal + ((3/100)*carRentalTotal)
+  under25cost = ((.3)*carRentalTotal)
 }
+let totalDueCost = (carRentalTotal + optionsCost + under25cost);
+//display at bottom
+carRentalCostParagraph.innerText = carRentalTotal;
+optionsCostParagraph.innerText = optionsCost;
+under25SurchargeParagraph.innerText = under25cost;
+totalDueParagraph.innerText = totalDueCost;
 
+}
 // alert
-alert("Car rental: $" + carRentalTotal.toFixed(2))
+//alert("Car rental: $" + carRentalTotal.toFixed(2))
 
-}
+
 //wire up
 estimatedCostButton.onclick = estimateTotalCost
